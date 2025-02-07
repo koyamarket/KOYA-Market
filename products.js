d3ce24e18bcbe71121068373ef0b7fc5663c14cc
koyamarket <republicofkoya@gmail.com>
2025-02-07 17:20:13 +0530

    Add files via upload

--- products.js
+++ products.js
@@ -1,26 +1,26 @@
 const products = [
     {
         id: 1,
-        name: "Product 1",
-        price: 19.99,
-        image: "assets/product1.jpg",
-        description: "Product 1 Description",
-        amazonUrl: "https://amazon.com/product1" // Add Amazon link here
+        name: "Strice Rollout Banner Pen",
+        price: "189/-",
+        image: "assets/Products/Copying pen 1.jpg",
+        description: "Blue Ink with  Pull-Out Rolling Paper - Ideal Exam Gadgets for Students and Office Work ",
+        amazonUrl: "https://www.amazon.in/Strice-Rollout-Banner-Pen-Pull-Out/dp/B0DNY14CM7?crid=197KNGDWG8HAB&dib=eyJ2IjoiMSJ9.mMNQcRnYwrhogdfOP9tihGKGEoDo_qWHNiEjpLU4b1HLYrC4yoT-5BEeLkZuwB5qohbGdqWugwA2-zFJqq4hP8qW_yYlT_B1CIYLNkyiN3iiu6PPk5RGq6znVApotIkEmqYLvYtXQmosVajPJ9kTW4GTbyn9XFo76e9x1H4lnf8gX_EYAa7xwtAQMUceEceoZ_tSFHymg3EOizQ-YtEZfo66V4VvlK5xjY55lEIediZJCF0Ja-QmQxUbjPgX73TDT1Aii07lQK9iF6XSoVbmnxxdean_HwqR4OaRv4s9tZ5fHXqp8dRWR2TU0Gpp5LNiuB1rlvTeDVm0FNumamm_8trom2nB8J7di3trAP7MSyw1WqZXmh3O4DjUgvtn3rc7v77xTe5XmxkQLIKtfNQbEnMGCQS4HKxRna84CGUSERApy6Wj0dpxn5swwmIJRQ-X.cxtGg3eiq4KbVfWJgG9Aky9GnP7Ot0nakmlbU_WavPU&dib_tag=se&keywords=exam%2Bcopy%2Bgadgets%2Bpen&qid=1738763629&sprefix=exam%2Bcopy%2B%2Caps%2C260&sr=8-5&th=1&linkCode=ll1&tag=koyamarket-21&linkId=f48d3c1ad28f883b0d89bc7327fd54b2&language=en_IN&ref_=as_li_ss_tl" // Add Amazon link here
     },
     {
         id: 2,
-        name: "Product 2",
-        price: 19.99,
-        image: "assets/product1.jpg",
-        description: "Product 1 Description",
-        amazonUrl: "https://amazon.com/product1" // Add Amazon link here
+        name: "GRECIILOOKS Trouser Pant for Men",
+        price: "479/-",
+        image: "assets/Products/Loose fit pants.jpg",
+        description: "Track Pant for Men Loose Fit | Sports Track Pant for Men | Track Pant Men | Men Stylish Track Pant",
+        amazonUrl: "https://www.amazon.in/GRECIILOOKS-Track-Trouser-Stylish-Regular/dp/B0DKP3V2DN?crid=1GXQJGHSVS61O&dib=eyJ2IjoiMSJ9.5Nrv2qdMhSDCeBh9S6rVVyctYeoHw5GfA3oQj6IeJhip5hAQz4jWIseFOXEl8a9_eRcAkbcA-Dfcd7CD_DwLYs_uig24D986FhaTJJJMQ2DtosT9D6qDHqdmV67EgK_6tLcKLeI1o7OROecmT2Eg6aSLN0jKwptGgMoYEpYZyisVX1nxm-nl8BCn7qItKYuSb65IzBGwNDzOzLu9VblIL2-Ce-_VjfR5rSMYNxo56i5VMyH6hw6noCJx2dhXzo4j7LAvtRxbMAC3QhUcQ0rj8Lv9gr_33RIZCZS1cJcDDmj5-Sc_oP-pjnbPpxjiSdy5N7bdhpobaj1NzKh7VHBWgKwcJJX8rqiRiI59NU9ZWxGtGik5kjBWZ3AweHYgYuTl95z5atTggJw0KN-TY0B1Wm46TFx6sNUJgKEmQ_HN0i0WWL2qCiHh2P4EMS8F4bA_.y3H9mcFO4hqUAqyqIga8V5ABox2U5INJhAHFdiM2Nmc&dib_tag=se&keywords=loose+fit+pants+for+men&qid=1738764825&sprefix=loose+fit+pants+for+%2Caps%2C678&sr=8-8&linkCode=ll1&tag=koyamarket-21&linkId=7d5f0c6377f349953c38c63ae02b572a&language=en_IN&ref_=as_li_ss_tl" // Add Amazon link here
     },
     {
         id: 3,
-        name: "Product 3",
-        price: 19.99,
-        image: "assets/product1.jpg",
-        description: "Product 1 Description",
-        amazonUrl: "https://amazon.com/product1" // Add Amazon link here
+        name: "CMF BY NOTHING Phone 1 5G",
+        price: "14,199/-",
+        image: "assets/Products/Nothing phone 1.jpg",
+        description: "5G (128 GB) (6 GB RAM) (Black) Free delevery, EMI Starts at ₹688. No Cost EMI available",
+        amazonUrl: "https://amzn.to/3CGnJBN" // Add Amazon link here
     }
   ];
--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
       productCard.innerHTML = `
         <img src="${product.image}" alt="${product.name}">
         <h3>${product.name}</h3>
-        <p>$${product.price}</p>
+        <p class="price">$${product.price}</p>
         <p>${product.description}</p>
         <a href="${product.amazonUrl}" target="_blank" class="btn">Buy Now</a>
       `;
