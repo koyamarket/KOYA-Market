a50aaa52f2b6821ba31967ff0460c32787b8ab20
koyamarket <republicofkoya@gmail.com>
2025-03-02 10:23:57 +0530

    Create realestate.js

---
+++ realestate.js
@@ -0,0 +1,26 @@
+const products = [
+    {
+        id: "nounash-apartment-vengara",
+        name: "NouNash Apartment in Vengara",
+        price: "4,499/month",
+        image: "/assets/Products/Nounash Apartments - Koya Market.jpg",
+        description: "Spacious 1BHK apartment with Parking. Near Shops And Masjids.located in Vengara, Malappuram.",
+        productUrl: "#"
+    },
+    {
+        id: "nounash-office-space-vengara",
+        name: "NouNash Office Space in Vengara",
+        price: "4,499/month",
+        image: "/assets/Products/Nounash Apartments - Koya Market.jpg",
+        description: "Standard Office Space with Parking, Near Shops And Masjids.located in Vengara, Malappuram.",
+        productUrl: "#"
+    },
+    {
+        id: "nounash-full-apartments-vengara",
+        name: "NouNash Full Apartment in Vengara",
+        price: "17,999/month",
+        image: "/assets/Products/Nounash Apartments - Koya Market.jpg",
+        description: "Full Apartment in 4 1BHK Houses in Vengara, Malappuram.",
+        productUrl: "#"
+    }
+];
